e7e81a684337233491ec897e2314fe25c10e9c46
yd8266uj <yd8266uj@metrostate.edu>
2016-10-31 06:51:39 -0500

    Update strings.php

--- classes/strings.php
+++ classes/strings.php
@@ -1,9 +1,9 @@
 <?php 
 
 class strings {
-	const DATABASE_CLASS__DATABASE_INFO = 'mysql:host=localhost;dbname=name_in_synonym';
-	const DATABASE_CLASS__DATABASE_USER = 'root';
-	const DATABASE_CLASS__DATABASE_PASSWORD = 'root';	 
+	const DATABASE_CLASS__DATABASE_INFO = 'mysql:host=localhost;dbname=ics499fa160124';
+	const DATABASE_CLASS__DATABASE_USER = 'ics499fa160124';
+	const DATABASE_CLASS__DATABASE_PASSWORD = '8784';	 
 }
 
 ?>
