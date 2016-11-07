5dcc4c61a62133a1199d499c47112674921be0b9
yd8266uj <yd8266uj@metrostate.edu>
2016-11-07 17:00:42 -0600

    b

--- classes/word.php
+++ classes/word.php
@@ -67,8 +67,8 @@ class word implements i_word {
   
   public function get_chars() {
     $chars = (new wordProcessor($this->word,$this->language))->getLogicalChars();
-    var_dump($chars);
-    return implode(';',json_encode($chars,JSON_UNESCAPED_UNICODE));
+    var_dump(json_encode($chars,JSON_UNESCAPED_UNICODE));
+    return implode(';',null);
   }
 
   /**
