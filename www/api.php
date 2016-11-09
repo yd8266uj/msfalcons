942af34e9ff5bc2fb7d391b96f3b9c6ffac24c56
yd8266uj <yd8266uj@metrostate.edu>
2016-11-09 03:36:20 -0600

    asd

--- www/api.php
+++ www/api.php
@@ -2,6 +2,10 @@
   ini_set('display_errors', 1);
   ini_set('display_startup_errors', 1);
   error_reporting(E_ALL);
+  
+  ini_set('default_charset','UTF-8');
+  ini_set('mbstring.internal_encoding','UTF-8');
+  
   include 'autoload.php';  
   
   switch(@$_POST['type']) {
